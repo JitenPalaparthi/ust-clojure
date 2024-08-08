0c1ba663c4de1c76230cc9b665e143ec694c59f2
Jiten <jpalaparthi@gmail.com>
2024-08-08 09:07:25 +0530

    add more tasks,nil and more

---
+++ demo17/src/demo17/core.clj
@@ -0,0 +1,46 @@
+(ns demo17.core
+  (:gen-class))
+(declare process-list)
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (def my-list1 `(100 200 300 nil 500))
+  (println my-list1)
+  (def num1 nil)
+  (println (nil? num1))
+  (println (some? my-list1)) ;; this returns true or false
+  (println (nil? my-list1))
+  (def my-list2 '()) ;; 
+  (def my-list3 nil) ;;nil variable 
+  (def my-set1 #{})
+  (println "is nil? " (nil? my-list2) my-list2)
+  (println "is there some value?" (some? my-list2))
+  (println "emoty set" my-set1)
+  (println "is the set nil?" (nil? my-set1))
+  (println "is the set some?" (some? my-set1))
+  
+  (println (process-list my-list1)) ; list with values and one of the values is nil
+  (println (process-list my-list2)) ; empty list which is not nil
+  (println (process-list my-list3)) ; nil list
+  
+  ;; can concatinate and format any kind of variable to str
+  (def str1 (str "the str is string formatter.It can foramt to any type of value .For instance int:" 100 " boolean:" true " float:" 123.12312 " list:" '(1,2,3) " set:" #{1,2,3} ))
+
+  (println str1 "type:" (type str1))
+  ;; (println "type:" (type my-list1))
+  ;; (println "type:" (type my-set1))
+  
+  )
+
+;; nil : when there is no value to a variable then it can be nil
+;; Default Value: A default value can be nil , even a function return type can be nil, to indicate no value is returned
+;; an empty list is different than nil
+
+;; What does this function return?
+;; What datatype value it returns?
+(defn process-list [lst]
+  (if (nil? lst)
+    "list is nil"
+    (str "List is:" lst)))
+
+
---
+++ tasks/10-task.md
@@ -0,0 +1,21 @@
+
+Take a list and a set of same size 
+
+
+list1 = 100, 200, nil, 400 , nil , 1 , nil
+set1 = 100 ,200 , 300 , nil, 400, nil, 500 , 600
+
+- write a function to accept list and set as input parameters and return the another list with the sum of list1 and set1 at each element level
+- where ever there is nil, consider it as 0
+- if both the list and set are nil, then return list with 0 (0)
+- if any of the list or set is nil then return only the list with values and also if there are any nils in the values of list or set , replace with 0 
+- make sure that the length of list and set are same. If not the remaining should be considerd as 0s. Example if list is 4 elements and set is 2 elements, then the remaining 2 elements to be added as 0
+
+- return list is : 200, 400  , 300,  400, 400, 1, 500 , 600
+
+(defn sum-list-set [list1 set1]
+;; the logic to add both the lists.
+list2
+)
+
+- to add new elements to the list (conj list elem)
