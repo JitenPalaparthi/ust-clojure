9d9fcaa5c2480993339fd893c21c428bd1d34608
Jiten <jpalaparthi@gmail.com>
2024-08-21 09:22:58 +0530

    add new example on thread

---
+++ demo27/src/demo27/core.clj
@@ -0,0 +1,20 @@
+(ns demo27.core
+  (:gen-class))
+(declare some-task)
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+  (let [thread1 (Thread. #(some-task "A"))
+        thread2 (Thread. #(some-task "B"))]
+    (.start thread1)
+    (.start thread2)
+    (.join thread1)
+    (.join thread2)
+    (println "Both the threads are called..")
+    ))
+
+(defn some-task [name]
+  (dotimes [i 5]
+    (println (str "Thread" name ":" i))
+    (Thread/sleep 1000)))
