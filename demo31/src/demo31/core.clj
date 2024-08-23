3f6add1808c7f5775c80ae0f4e490fa6399df301
Jiten <jpalaparthi@gmail.com>
2024-08-23 09:28:28 +0530

    add more examples on promises

---
+++ demo31/src/demo31/core.clj
@@ -0,0 +1,27 @@
+(ns demo31.core
+  (:gen-class))
+
+
+(defn create-vector []
+  (Thread/sleep 2000)
+  [1,2,3,4])
+
+(defn double-vector [vec]
+  (mapv #(* 2 %) vec)
+  )
+
+(defn -main
+  [& args]
+  
+  (let [vector-promise (promise)] 
+    (future 
+      (let [vec (create-vector)]
+        (deliver vector-promise vec))) 
+    (let [original-vec @vector-promise
+          double-vec (double-vector original-vec)
+          ]
+      (println "original vector:" original-vec)
+      (println "double-vector:" double-vec)
+      ) 
+    ) 
+  )
---
+++ demo31/test/demo31/core_test.clj
@@ -0,0 +1,7 @@
+(ns demo31.core-test
+  (:require [clojure.test :refer :all]
+            [demo31.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
