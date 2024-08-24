8c1e8a10c2776eefb809eab2b14c059476a7d0da
Jiten <jpalaparthi@gmail.com>
2024-08-24 12:11:30 +0530

    add more examples on userdefined types and protocols

--- demo35/src/demo35/core.clj
+++ demo35/src/demo35/core.clj
@@ -1,7 +1,31 @@
 (ns demo35.core
   (:gen-class))
 
+(defprotocol Area
+  (area [this]))
+
+(defprotocol Perimeter
+  (perimeter [this]))
+
+(deftype Rectangle [l b]
+  Area
+  (area [this]
+    (* l b))
+    Object
+  (toString [this]
+            (str "Length:" (.l this) " Bredth:" (.b this)))
+  )
+
+(extend-type Rectangle
+  Perimeter
+  (perimeter [this]
+    (* 2 (+ (.l this) (.b this))))
+  )
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (def rec (Rectangle. 10 12.45))
+  (println "area of rect:" (area rec))
+  (println "perimeter of rect:" (perimeter rec))
+  (println "To String:" (.toString rec))
+  )
